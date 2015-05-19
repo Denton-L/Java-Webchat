0fe7f73e787ee8016a7a62a0580b10060b018827
Denton-L <liu.denton+github@gmail.com>
2015-05-19 11:25:40 -0400

    Initial commit.

---
+++ Webchat/WebChat/src/networking/Message.java
@@ -0,0 +1,28 @@
+package networking;
+
+import java.io.Serializable;
+
+public class Message implements Serializable {
+	private String content;
+	private String username;
+	private long timestamp;
+	
+	public Message(String content, String username, long timestamp) {
+		this.content = content;
+		this.username = username;
+		this.timestamp = timestamp;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public long getTimestamp() {
+		return timestamp;
+	}
+	
+}
