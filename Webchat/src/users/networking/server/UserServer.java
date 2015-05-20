185572636bec61d09fbb7a0fe366a21adf89789f
Denton-L <liu.denton+github@gmail.com>
2015-05-20 06:04:16 -0400

    Updated server locations

--- Webchat/src/users/networking/server/UserServer.java
+++ Webchat/src/users/networking/server/UserServer.java
@@ -1,4 +1,4 @@
-package message.networking.server;
+package users.networking.server;
 
 import java.util.SortedSet;
 import java.util.TreeSet;
@@ -6,7 +6,7 @@ import java.util.TreeSet;
 import message.Message;
 import message.MessageComparator;
 
-public class MessageServer {
+public class UserServer {
 	
-	public static final String URL_LOCATION = "/messages";
+	public static final String URL_LOCATION = "/users";
 }
