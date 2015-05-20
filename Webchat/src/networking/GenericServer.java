40ab176fd9aff11776fb0918758847ae074772ca
Denton Liu <liu.denton+github@gmail.com>
2015-05-20 06:20:23 -0400

    Created GenericServer

---
+++ Webchat/src/networking/GenericServer.java
@@ -0,0 +1,26 @@
+package networking;
+
+import java.net.MalformedURLException;
+import java.rmi.AlreadyBoundException;
+import java.rmi.Naming;
+import java.rmi.RMISecurityManager;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public class GenericServer {
+	
+	private String location;
+	private Remote binding;
+	
+	public GenericServer(String location, Remote binding) {
+		this.location = location;
+		this.binding = binding;
+	}
+	
+	public void startServer() throws MalformedURLException, RemoteException, AlreadyBoundException {
+		System.setSecurityManager(new RMISecurityManager());
+		
+		Naming.bind("rmi://localhost/" +location, binding);
+		
+	}
+}
