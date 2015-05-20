55dab609bbe7e902250f1d7a439880b60e5a31d7
Denton Liu <liu.denton+github@gmail.com>
2015-05-20 06:03:03 -0400

    Created UserService

---
+++ Webchat/src/users/networking/server/UserService.java
@@ -0,0 +1,31 @@
+package users.networking.server;
+
+import java.io.Serializable;
+
+import users.networking.UserInterface;
+
+public class UserService implements UserInterface, Serializable {
+	
+	@Override
+	public boolean register(String username, byte[] password) {
+		// TODO interface with database
+		// remember to hash the password once before sending it off!
+		return false;
+	}
+	
+	@Override
+	public String signIn(String username, byte[] password) {
+		// TODO Auto-generated method stub
+		// remember to hash the password once before sending it off!
+		//make the modhash a securerandom string
+		return null;
+	}
+
+	@Override
+	public void logout(String username, byte[] password) {
+		// TODO Auto-generated method stub
+		//remove the user's instancehash
+		
+	}
+	
+}
