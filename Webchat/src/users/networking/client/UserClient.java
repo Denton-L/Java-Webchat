3b51381851301cfb6855a122e76f44bce2e51fbf
Denton Liu <liu.denton+github@gmail.com>
2015-05-20 06:52:03 -0400

    Made UserClient wrap around UserInterface

--- Webchat/src/users/networking/client/UserClient.java
+++ Webchat/src/users/networking/client/UserClient.java
@@ -5,14 +5,32 @@ import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 
 import networking.GenericClient;
+import users.PasswordManager;
 import users.networking.UserInterface;
 import users.networking.server.UserServer;
 
 public class UserClient extends GenericClient {
 	private UserInterface userInterface;
+	private String instanceHash;
 	
 	public UserClient(String serverIp) throws MalformedURLException,
 			NotBoundException, RemoteException {
 		super(serverIp, UserServer.URL_LOCATION);
+	}
+	
+	public boolean register(String username, byte[] password) {
+		byte[] hashedPass = PasswordManager.clientHash(password, username);
+		PasswordManager.clearArray(password);
+		return userInterface.register(username, hashedPass);
+	}
+	
+	public String signIn(String username, byte[] password) {
+		byte[] hashedPass = PasswordManager.clientHash(password, username);
+		PasswordManager.clearArray(password);
+		return instanceHash = userInterface.signIn(username, hashedPass);
+	}
+	
+	public void logout(String instanceHash) {
+		userInterface.logout(instanceHash);
 	}
 }
