358a7b081028dc5b35a55297ec5bd8e62465fd1a
dantun-leu <denton.liu@student.tdsb.on.ca>
2015-05-20 06:38:16 -0400

    Created GenericClient and made UserClient and MessageClient inherit from them

--- Webchat/src/users/networking/client/UserClient.java
+++ Webchat/src/users/networking/client/UserClient.java
@@ -1,22 +1,18 @@
 package users.networking.client;
 
 import java.net.MalformedURLException;
-import java.rmi.Naming;
 import java.rmi.NotBoundException;
-import java.rmi.RMISecurityManager;
 import java.rmi.RemoteException;
 
+import networking.GenericClient;
 import users.networking.UserInterface;
 import users.networking.server.UserServer;
 
-public class UserClient {
+public class UserClient extends GenericClient {
 	private UserInterface userInterface;
 	
-	public UserClient(String serverIp, String userHash)
-			throws MalformedURLException, NotBoundException, RemoteException {
-		
-		System.setSecurityManager(new RMISecurityManager());
-		this.userInterface = (UserInterface) Naming.lookup(serverIp + "/"
-				+ UserServer.URL_LOCATION);
+	public UserClient(String serverIp) throws MalformedURLException,
+			NotBoundException, RemoteException {
+		super(serverIp, UserServer.URL_LOCATION);
 	}
 }
