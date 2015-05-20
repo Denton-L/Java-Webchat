adced8a85ef39e1673a2e80d48ef87c036db0cd1
Denton Liu <liu.denton+github@gmail.com>
2015-05-20 05:09:08 -0400

    Change the MessageInterface to accept a String message and a userhash

--- Webchat/src/networking/MessageInterface.java
+++ Webchat/src/networking/MessageInterface.java
@@ -4,7 +4,7 @@ import java.rmi.Remote;
 import java.util.SortedSet;
 
 public interface MessageInterface extends Remote {
-	public void push(Message message);
+	public void push(String message, String userHash);
 	
 	public SortedSet<Message> pull(Message lastMessageRecieved);
 }
