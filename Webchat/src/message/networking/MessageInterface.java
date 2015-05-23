44325f8b532645e9aa605c6e30ef0c40e0381c13
Denton Liu <liu.denton+github@gmail.com>
2015-05-23 09:06:26 -0400

    Javadoc for MessageInterface

--- Webchat/src/message/networking/MessageInterface.java
+++ Webchat/src/message/networking/MessageInterface.java
@@ -5,8 +5,35 @@ import java.util.SortedSet;
 
 import message.Message;
 
+/**
+ * An abstract interface which will allow messages to be sent between the client
+ * and the server through the RMI protocol.
+ * 
+ * @author Filip Francetic
+ * @author Denton Liu
+ * @version 2015-05-23
+ */
 public interface MessageInterface extends Remote {
+
+	/**
+	 * Pushes a message to the server.
+	 * 
+	 * @param content
+	 *            The message content that will be pushed.
+	 * @param userInstance
+	 *            The {@code byte[]} which uniquely identifies a user.
+	 */
 	void push(String content, byte[] userInstance);
-	
+
+	/**
+	 * Pulls all messages received since the last message received.
+	 * 
+	 * @param lastMessageRecieved
+	 *            The {@code Message} which was the last one received by the
+	 *            client. Pass {@code null} if no messages have been received
+	 *            yet.
+	 * @return A {@code SortedSet<Message>} ordered in ascending order of their
+	 *         timestamps from when the last message was received.
+	 */
 	SortedSet<Message> pull(Message lastMessageRecieved);
 }
