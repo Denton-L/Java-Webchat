762edb176f9c642b567b07b6dc04d72997da980d
dantun-leu <denton.liu@student.tdsb.on.ca>
2015-05-25 11:11:41 -0400

    Javadoc MessageServer

--- Webchat/src/webchat/message/networking/server/MessageServer.java
+++ Webchat/src/webchat/message/networking/server/MessageServer.java
@@ -2,8 +2,15 @@ package webchat.message.networking.server;
 
 import webchat.networking.GenericServer;
 
+/**
+ * A server that handles the transmission of messages.
+ * 
+ * @author Denton Liu
+ * @version 2015-05-25
+ */
 public class MessageServer extends GenericServer {
 	
+	/** The subdomain location of the server. */
 	public static final String URL_LOCATION = "messages";
 	
 	public MessageServer() {
