d097df43c7567f786d3eeda695d0f4b5fa9f246b
dantun-leu <denton.liu@student.tdsb.on.ca>
2015-05-25 22:05:06 -0400

    Javadoc GenericServer

--- Webchat/src/webchat/networking/GenericServer.java
+++ Webchat/src/webchat/networking/GenericServer.java
@@ -8,24 +8,43 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 /**
+ * A generic RMI server from which other clients will inherit.
  * 
  * @author Denton Liu
- * @2015-05-25
+ * @version 2015-05-25
  */
 public abstract class GenericServer {
 	
+	/** The location of the server. */
 	private String location;
+	/** The {@code Remote} that will be bound to this server. */
 	private Remote binding;
 	
+	/**
+	 * 
+	 * @param location
+	 *            The location of binding on the server.
+	 * @param binding
+	 *            The {@code Remote} that will be bound to this server.
+	 */
 	public GenericServer(String location, Remote binding) {
 		this.location = location;
 		this.binding = binding;
 	}
 	
-	public void startServer() throws MalformedURLException, RemoteException, AlreadyBoundException {
+	/**
+	 * Starts the server and binds the {@code Remote} object to the specified
+	 * location.
+	 * 
+	 * @throws MalformedURLException
+	 * @throws RemoteException
+	 * @throws AlreadyBoundException
+	 */
+	public void startServer() throws MalformedURLException, RemoteException,
+			AlreadyBoundException {
 		System.setSecurityManager(new RMISecurityManager());
 		
-		Naming.bind("rmi://localhost/" +location, binding);
+		Naming.bind("rmi://localhost/" + location, binding);
 		
 	}
 }
