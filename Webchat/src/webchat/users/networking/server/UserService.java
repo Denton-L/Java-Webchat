508bd5b5e9bf140e594dbae3a7d81c9f1f31719a
Denton Liu <liu.denton+github@gmail.com>
2015-05-28 12:57:31 -0400

    Finished logic of UserDatabase, changed the logic of classes dependant on it and corrected/created JavaDocs

--- Webchat/src/webchat/users/networking/server/UserService.java
+++ Webchat/src/webchat/users/networking/server/UserService.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.rmi.RemoteException;
 import java.security.SecureRandom;
 
+import webchat.database.UserDatabase;
 import webchat.users.PasswordManager;
 import webchat.users.User;
 import webchat.users.networking.UserInterface;
@@ -15,10 +16,23 @@ import webchat.users.networking.UserInterface;
  * @version 2015-05-25
  */
 public class UserService implements UserInterface, Serializable {
-	
+
 	/** The length of the uniquely identifying {@code byte[]}. */
 	private final static int USER_INSTANCE_LENGTH = 0xFF;
-	
+
+	/** The {@code UserDatabase} which this {@code UserService} is based on. */
+	private UserDatabase userDatabase;
+
+	/**
+	 * 
+	 * @param userDatabase
+	 *            The {@code UserDatabase} which this {@code UserService} is
+	 *            based on.
+	 */
+	public UserService(UserDatabase userDatabase) {
+		this.userDatabase = userDatabase;
+	}
+
 	/**
 	 * Returns a {@code User} from a {@code username} and {@code password}. Note
 	 * that the password is hashed automatically.
@@ -33,33 +47,35 @@ public class UserService implements UserInterface, Serializable {
 		return new User(username, PasswordManager.serverHash(passwordHash,
 				username));
 	}
-	
+
 	@Override
-	public boolean register(String username, byte[] passwordHash) throws RemoteException {
+	public boolean register(String username, byte[] passwordHash)
+			throws RemoteException {
 		User user = userFromNameAndPassword(username, passwordHash);
-		if (true /* doesn't exist in database */) {
-			// add to database
-			return true;
-		} else {
-			return false;
-		}
-		
+		return userDatabase.addUser(username, passwordHash);
+
 	}
-	
+
 	@Override
-	public byte[] signIn(String username, byte[] passwordHash) throws RemoteException {
-		User user = userFromNameAndPassword(username, passwordHash);
+	public byte[] signIn(String username, byte[] passwordHash)
+			throws RemoteException {
+
 		byte[] userInstance = new byte[USER_INSTANCE_LENGTH];
-		// get user from database
-		new SecureRandom().nextBytes(userInstance);
-		// store userInstance in database
-		return userInstance;
+
+		if (userDatabase.isCorrectUserAndPassword(username, passwordHash)) {
+			new SecureRandom().nextBytes(userInstance);
+			userDatabase.setUserInstance(username, userInstance);
+			return userInstance;
+		} else {
+			return null;
+		}
 	}
-	
+
 	@Override
 	public void logout(byte[] userInstance) throws RemoteException {
-		// remove userInstance from database
-		
+		userDatabase.setUserInstance(
+				userDatabase.getUsernameFromUserInstance(userInstance), null);
+
 	}
-	
+
 }
