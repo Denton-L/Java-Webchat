f1e0a9b2b01af3bec3f3d786c74fdc710c38738e
Filip-F <f.francet@hotmail.com>
2015-05-30 07:35:14 -0400

    Integrated GUI and Database.

--- Webchat/src/webchat/ui/MainInterface.java
+++ Webchat/src/webchat/ui/MainInterface.java
@@ -1,6 +1,13 @@
 package webchat.ui;
 
+import java.net.MalformedURLException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+import webchat.users.networking.client.UserClient;
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
@@ -8,56 +15,102 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+/**
+ * Main Interface object
+ * 
+ * Takes care of interaction between GUI objects and event handling.
+ * 
+ * @author Jing Yi Xie
+ * @version 1.0
+ */
 
 public class MainInterface extends Application {
 	String user;
 	String newUser;
 	String newPass;
+	final ServScene servScene = new ServScene();
 	final ChatScene chatScene = new ChatScene();
 	final RegScene regScene = new RegScene();
 	final MsgScene msgScene = new MsgScene();
-	
+
 	static Font f = Font.loadFont(
-			MainInterface.class.getResource(
-					"/ufonts.com_segoe-ui-light.ttf").toExternalForm(), 17);
+			MainInterface.class.getResource("/res/SegoeUILight.ttf")
+					.toExternalForm(), 17);
 	static Font fLarge = Font.loadFont(
-			MainInterface.class.getResource(
-					"/ufonts.com_segoe-ui-light.ttf").toExternalForm(), 25);
+			MainInterface.class.getResource("/res/SegoeUILight.ttf")
+					.toExternalForm(), 25);
 	static Font fBig = Font.loadFont(
-			MainInterface.class.getResource(
-					"/ufonts.com_segoe-ui-light.ttf").toExternalForm(), 45);
-	
+			MainInterface.class.getResource("/res/SegoeUILight.ttf")
+					.toExternalForm(), 45);
+	static Font font = Font.loadFont(
+			MainInterface.class.getResource("/res/AvenirLTStd-Light.otf")
+					.toExternalForm(), 17);
+	static Font font2 = Font.loadFont(
+			MainInterface.class.getResource("/res/NexaLight.otf")
+					.toExternalForm(), 17);
+
 	UserUpdate userupd = new UserUpdate(msgScene);
 	Messenger messenger = new Messenger(msgScene);
-	
+	UserInfo userinfo = new UserInfo();
+	ServerAddr servaddr = new ServerAddr(servScene);
+
+	UserClient client;
+	byte[] userInstance;
+
+	/**
+	 * Main method used to run the applications.
+	 * 
+	 * @param Unused
+	 *            .
+	 * @return Nothing.
+	 */
+
 	public static void main(String[] args) {
-		System.out.println("asdf");
+		System.out.println(font.getName());
 		launch(args);
 	}
-	
-	@Override
+
+	/**
+	 * This is used to set up the application by adding event handlers to the
+	 * appropriate elements and changing the scene when necessary.
+	 */
 	public void start(final Stage primaryStage) {
-		Font.loadFont(MainInterface.class.getResource("/NexaLight.otf")
-				.toExternalForm(), 17);
-		
+
+		final Scene serv = servScene.createServ();
 		final Scene chat = chatScene.createChat();
 		final Scene reg = regScene.createReg();
 		final Scene msg = msgScene.createMsg();
+
 		
-		primaryStage.setTitle("Chat");
-		primaryStage.setScene(chat);
+		
+		primaryStage.setTitle("Web Chat");
+		primaryStage.setScene(serv);
+		primaryStage.setResizable(false);
+		primaryStage.setWidth(serv.getWidth());
+		primaryStage.setHeight(serv.getHeight());
 		primaryStage.show();
-		
+
+		messenger.writeMsg("hello", "hello", "hello");
 		userupd.writeUser("lucychoi");
 		userupd.writeUser("cindypiao");
-		messenger
-				.writeMsg(
-						"sample user",
-						"lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mauris neque, imperdiet ac porta ut, feugiat a quam. Praesent laoreet sapien sem, et sodales arcu vulputate eget.",
-						"sample time");
 		userupd.userStatus("cindypiao", true);
-		
+
+		primaryStage.heightProperty().addListener(new ChangeListener<Object>() {
+			@Override
+			public void changed(ObservableValue<?> observable, Object oldvalue,
+					Object newValue) {
+				msgScene.imview.setFitHeight(primaryStage.getHeight());
+				msgScene.imview.setFitWidth(primaryStage.getWidth());
+				regScene.imview.setFitHeight(primaryStage.getHeight());
+				regScene.imview.setFitWidth(primaryStage.getWidth());
+				chatScene.imview.setFitHeight(primaryStage.getHeight());
+				chatScene.imview.setFitWidth(primaryStage.getWidth());
+			}
+		});
+
 		chatScene.register.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
@@ -65,77 +118,87 @@ public class MainInterface extends Application {
 				primaryStage.setScene(reg);
 			}
 		});
-		
+
+		servScene.enter.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				try {
+					client = new UserClient(servScene.ipBox.getText());
+					servaddr.getAddress();
+					servScene.clrAll();
+					primaryStage.setScene(chat);
+				} catch (MalformedURLException | RemoteException | NotBoundException e) {
+					servScene.warning.setVisible(true);
+				}
+			}
+		});
+
 		chatScene.enter.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
-				UserLogin userlogin = new UserLogin(
-						chatScene.userBox.getText(), chatScene.pwBox.getText(),
-						chatScene.ipBox.getText());
-				if (userlogin.infoCheck() == true) {
-					messenger.getUser(chatScene.userBox.getText());
-					System.out.println(chatScene.userBox.getText());
-					primaryStage.setScene(msg);
-					chatScene.clrAll();
-					chatScene.warning.setVisible(false);
-				} else
+				try {
+					userInstance = client.signIn(chatScene.userBox.getText(),
+							chatScene.pwBox.getText().getBytes());
+					if (userInstance != null) {
+						primaryStage.setScene(msg);
+						chatScene.clrAll();
+					} else
+						chatScene.warning.setVisible(true);
+				} catch (RemoteException e) {
 					chatScene.warning.setVisible(true);
+				}
 			}
 		});
-		
+
 		regScene.enter.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
-				UserInfo userinfo = new UserInfo(regScene.userBox.getText(),
-						regScene.pwBox.getText(), regScene.pwBox2.getText());
-				if (userinfo.userpasscheck() == true) {
-					primaryStage.setScene(chat);
-					regScene.clrAll();
-					// call userinfo to get new user info
-				} else
-					regScene.warning.setVisible(true);
+				if (regScene.pwBox.getText().equals(regScene.pwBox2.getText())) {
+					try {
+						if (client.register(regScene.userBox.getText(),
+								regScene.pwBox.getText().getBytes())) {
+							primaryStage.setScene(chat);
+							regScene.clrAll();
+						} else
+							regScene.warning.setVisible(true);
+					} catch (RemoteException e) {
+						regScene.warning.setVisible(true);
+					}
+				}
 			}
 		});
-		
+
 		msgScene.logout.setOnMousePressed(new EventHandler<MouseEvent>() {
 			@Override
 			public void handle(MouseEvent mouseEvent) {
-				primaryStage.setScene(chat);
+				primaryStage.setScene(serv);
 				msgScene.clrAll();
 			}
-			
+
 		});
-		
+
 		msgScene.enter.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent actionEvent) {
-				messenger.deliverMsg();
+				if (msgScene.input.getText() != null
+						&& !msgScene.input.getText().trim().isEmpty())
+					messenger.deliverMsg();
 			}
 		});
-		
+
 		msgScene.input.addEventFilter(KeyEvent.KEY_PRESSED,
 				new EventHandler<KeyEvent>() {
 					@Override
 					public void handle(KeyEvent ke) {
 						if (ke.getCode().equals(KeyCode.ENTER)) {
-							messenger.deliverMsg();
+							if (msgScene.input.getText() != null
+									&& !msgScene.input.getText().trim()
+											.isEmpty())
+								messenger.deliverMsg();
 							// call messenger for message;
 							ke.consume();
 						}
 					}
 				});
-		
-	}
-	
-	public static Font getFont() {
-		return f;
-	}
-	
-	public static Font getFont2() {
-		return fLarge;
-	}
-	
-	public static Font getFont3() {
-		return fBig;
 	}
 }
--- Webchat/src/webchat/ui/RegScene.java
+++ Webchat/src/webchat/ui/RegScene.java
@@ -7,14 +7,20 @@ import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundImage;
-import javafx.scene.layout.BackgroundPosition;
-import javafx.scene.layout.BackgroundRepeat;
-import javafx.scene.layout.BackgroundSize;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+
+/**
+ * RegScene Object
+ * 
+* Creates and deals with the registration scene. 
+*  
+* @author Jing Yi Xie
+* @version 1.0
+*/
 
 public class RegScene {
 	Text warning = new Text("Error with input information!");
@@ -27,55 +33,62 @@ public class RegScene {
 	PasswordField pwBox2 = new PasswordField();
 	Button enter = new Button();
 	GridPane pane = new GridPane();
-	
+	Image image1 = new Image("/res/plshelp.jpg", true);
+	ImageView imview = new ImageView();
+
+	/**
+	 * Sets up elements as necessary in order to create the registration scene.
+	 * @return A Scene object.
+	 */
 	public Scene createReg() {
-		// background
-		Image image1 = new Image("/plshelp.jpg", true);
-		BackgroundSize bgsize = new BackgroundSize(800, 600, false, false,
-				true, true);
 		
-		BackgroundImage myBI = new BackgroundImage(image1,
-				BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT,
-				BackgroundPosition.DEFAULT, bgsize);
+		StackPane group = new StackPane();		
+		imview.setFitHeight(600);
+	    imview.setFitWidth(800);
+        imview.setImage(image1);
+		group.getChildren().add(imview);
 		
-		// create BorderPane
 		pane.setPadding(new Insets(10, 15, 10, 15));
-		pane.setBackground(new Background(myBI));
 		pane.setAlignment(Pos.CENTER);
 		pane.setVgap(10);
 		pane.setHgap(10);
-		
+
 		title1.setId("welcome");
 		warning.setId("textstyle2");
-		
+
 		userBox.setPrefWidth(350);
 		userBox.setPromptText("Username");
-		userBox.setPrefHeight(40);
-		userBox.setFont(MainInterface.getFont());
-		
+		userBox.setPrefHeight(45);
+
 		pwBox.setPromptText("Password");
-		pwBox.setFont(MainInterface.getFont());
-		
+		pwBox.setPrefHeight(45);
+
 		pwBox2.setPromptText("Confirm Password");
-		pwBox2.setFont(MainInterface.getFont());
-		
+		pwBox2.setPrefHeight(45);
+
 		enter.setText(">");
-		
+		enter.setPrefSize(50, 50);
+
 		pane.add(title1, 0, 0);
 		pane.add(userBox, 0, 1);
 		pane.add(pwBox, 0, 2);
 		pane.add(pwBox2, 0, 3);
 		pane.add(enter, 1, 3);
 		pane.add(warning, 0, 4);
-		
+
 		warning.setVisible(false);
 		
+		group.getChildren().add(pane);
+
 		// create scene
-		Scene scene = new Scene(pane, 800, 600);
-		scene.getStylesheets().add("/custom.css");
+		Scene scene = new Scene(group, 800, 600);
+		scene.getStylesheets().add("/res/custom.css");
 		return scene;
 	}
-	
+
+	/**
+	 * Clears text for next messaging session.
+	 */
 	public void clrAll() {
 		userBox.setText("");
 		pwBox2.setText("");
