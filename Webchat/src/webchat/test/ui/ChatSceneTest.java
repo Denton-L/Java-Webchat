b209ff8b5dbc2de71272369e301d83d4690fabe1
Filip-F <f.francet@hotmail.com>
2015-05-31 03:07:00 -0400

    Added test classes (to be fixed soon)

---
+++ Webchat/src/webchat/test/ui/ChatSceneTest.java
@@ -0,0 +1,15 @@
+package webchat.test.ui;
+
+import junit.framework.TestCase;
+
+public class ChatSceneTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
