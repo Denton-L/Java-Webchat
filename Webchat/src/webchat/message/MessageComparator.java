5e22cd0a0e6b5a0accc8ebce77e48151bfae146a
Filip-F <f.francet@hotmail.com>
2015-05-31 18:11:15 -0400

    Updated with Denton's files.

--- Webchat/src/webchat/message/MessageComparator.java
+++ Webchat/src/webchat/message/MessageComparator.java
@@ -18,9 +18,16 @@ public class MessageComparator implements Comparator<Message>, Serializable {
 	 */
 	@Override
 	public int compare(Message m1, Message m2) {
-		if (m1 == null || m2 == null) {
+		if (m1 == null && m2 == null) {
+			return 0;
+		}
+		if (m1 == null) {
 			return -1;
 		}
+		if (m2 == null) {
+			return 1;
+		}
+		
 		return (int) (m1.getTimestamp() - m2.getTimestamp());
 	}
 }
