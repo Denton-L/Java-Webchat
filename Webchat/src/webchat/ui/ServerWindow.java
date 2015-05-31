43aaa648660e9f17e04e8aa2aa5bfe1d09ea0ff5
Martingrove-Guy <filip.francet@gmail.com>
2015-05-31 01:48:30 -0400

    Updated server GUI.

--- Webchat/src/webchat/ui/ServerWindow.java
+++ Webchat/src/webchat/ui/ServerWindow.java
@@ -8,8 +8,12 @@ import java.net.UnknownHostException;
 import java.io.IOException;
 import java.rmi.AlreadyBoundException;
 
+import javax.imageio.ImageIO;
+
+import webchat.database.UserDatabase;
 import webchat.networking.StartServer;
 import javafx.application.Application;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
@@ -37,14 +41,12 @@ public class ServerWindow extends Application {
 
 	Text fileText;
 	Text errorText;
-
+	Text selectText;
+	Text createText;
 	File database;
 
 	public void start(final Stage primaryStage) {
-		
-		//TODO check if this works for runnable jars
-		fileChooser.setInitialDirectory(new File(System.getProperty("user.dir") + "/src/res"));
-		
+
 		StackPane group = new StackPane();
 		imview.setImage(image1);
 		imview.setFitHeight(600);
@@ -52,15 +54,16 @@ public class ServerWindow extends Application {
 
 		VBox vbox = new VBox();
 		vbox.setSpacing(15);
-		
+
 		HBox hbox1 = new HBox();
 		hbox1.setSpacing(15);
-		
+
 		HBox hbox2 = new HBox();
 		hbox2.setSpacing(15);
 
-		
-		
+		HBox hbox3 = new HBox();
+		hbox3.setSpacing(15);
+
 		GridPane pane = new GridPane();
 		pane.setAlignment(Pos.CENTER);
 		pane.add(vbox, 1, 1);
@@ -83,35 +86,54 @@ public class ServerWindow extends Application {
 		fileText = new Text("No file selected");
 		fileText.setId("textstyle2");
 		hbox1.getChildren().add(fileText);
-		
-		vbox.getChildren().addAll(hbox1,hbox2,startServer);
 
-		
+		createText = new Text("");
+		createText.setVisible(false);
+		createText.setId("textstyle2");
+		hbox2.getChildren().add(createText);
+
+		selectText = new Text("Select a file before starting the server");
+		selectText.setId("textstyle2");
+
+		vbox.getChildren().addAll(hbox1, hbox2, hbox3);
 		startServer.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(final ActionEvent e) {
-				try {
-					StartServer.start(database);
-				} catch (FileNotFoundException e1) {
-					errorText.setText("File could not be found");
-					errorText.setVisible(true);
-				} catch (ClassNotFoundException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				} catch (IOException e1) {
-					errorText.setText("Database could not be loaded");
-					errorText.setVisible(true);
-				} catch (AlreadyBoundException e1) {
-					errorText.setText("Server could not be started");
+				if (database != null) {
+					try {
+						StartServer.start(database);
+						selectText.setText("Server started");
+					} catch (FileNotFoundException e1) {
+						errorText.setText("File could not be found");
+						errorText.setVisible(true);
+						e1.printStackTrace();
+					} catch (ClassNotFoundException e1) {
+						e1.printStackTrace();
+					} catch (IOException e1) {
+						errorText.setText("Database could not be loaded");
+						errorText.setVisible(true);
+						e1.printStackTrace();
+					} catch (AlreadyBoundException e1) {
+						errorText.setText("Server could not be started");
+						errorText.setVisible(true);
+						e1.printStackTrace();
+					}
+				} else {
+					errorText.setText("No file selected");
 					errorText.setVisible(true);
 				}
 			}
 		});
 
+
+		hbox3.getChildren().add(startServer);
+
 		errorText = new Text("");
 		errorText.setVisible(false);
 		errorText.setId("textstyle2");
-		vbox.getChildren().add(errorText);
+		hbox3.getChildren().add(errorText);
+
+		vbox.getChildren().add(selectText);
 
 		final Scene scene = new Scene(group, 500, 250);
 
@@ -122,13 +144,62 @@ public class ServerWindow extends Application {
 	}
 
 	private void addFileChooser(final Stage stage, HBox hbox1, HBox hbox2) {
+		// TODO check if this works for runnable jars
+		fileChooser.setInitialDirectory(new File(System.getProperty("user.dir")
+				+ "/src/res"));
+		fileChooser.getExtensionFilters().add(
+				new FileChooser.ExtensionFilter("SER file", "*.ser"));
 
 		openDatabase.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(final ActionEvent e) {
+				fileChooser.setTitle("Select Database file");
 				database = fileChooser.showOpenDialog(stage);
-				if (database != null)
+				if (database != null) {
 					fileText.setText("File chosen: " + database.getName());
+					selectText.setText("");
+				}
+			}
+		});
+
+		createDatabase.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(final ActionEvent e) {
+				fileChooser.setTitle("Create Database");
+				database = fileChooser.showSaveDialog(stage);
+				if (database != null) {
+					int i = database.toString().lastIndexOf('.');
+					int p = Math.max(database.toString().lastIndexOf('/'),
+							database.toString().lastIndexOf('\\'));
+					if (i < p) {
+						database = new File(database.toString() + ".ser");
+					}
+					if (database
+							.toString()
+							.substring(database.toString().lastIndexOf('.') + 1)
+							.equals("ser")) {
+
+						try {
+							new UserDatabase().saveDatabase(database);
+							createText.setText("Database file created");
+							createText.setVisible(true);
+							fileText.setText("File chosen: "
+									+ database.getName());
+							selectText.setText("");
+						} catch (FileNotFoundException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						} catch (IOException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+					} else {
+						createText.setText("Incorrect file format");
+						createText.setVisible(true);
+					}
+
+				}
+
 			}
 		});
 
