b56e79438c6decc63e58be54cfdb2dc438c8146e
Denton Liu <liu.denton+github@gmail.com>
2015-05-31 22:42:06 -0400

    Moved tests into test folder

---
+++ Webchat/tests/webchat/test/users/networking/client/UserClientTest.java
@@ -0,0 +1,34 @@
+package webchat.test.users.networking.client;
+
+import java.io.File;
+import java.net.InetAddress;
+
+import webchat.database.UserDatabase;
+import webchat.networking.StartServer;
+import webchat.users.networking.client.UserClient;
+import webchat.users.networking.server.UserServer;
+import junit.framework.TestCase;
+
+public class UserClientTest extends TestCase {
+	UserServer server;
+	UserClient client;
+	boolean setUpIsDone = true;
+	
+	protected void setUp() throws Exception {
+		if (setUpIsDone) {
+			UserDatabase database = new UserDatabase(new File("/database.ser"));
+			UserServer server = new UserServer(database);
+			server.startServer();
+			// client = new
+			// UserClient(InetAddress.getLocalHost().getHostAddress());
+			// TODO fix this
+		}
+		
+		setUpIsDone = false;
+	}
+	
+	public void testCreation() {
+		// Note is blank because only needs to be created
+	}
+	
+}
