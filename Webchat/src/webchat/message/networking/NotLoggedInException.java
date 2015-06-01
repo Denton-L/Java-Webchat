c0ae7752a6ddb90ff14d6691d0ed6a2df61fa0de
Denton Liu <liu.denton+github@gmail.com>
2015-06-01 01:34:15 -0400

    More javadoc updates.

--- Webchat/src/webchat/message/networking/NotLoggedInException.java
+++ Webchat/src/webchat/message/networking/NotLoggedInException.java
@@ -1,7 +1,14 @@
 package webchat.message.networking;
 
+/**
+ * Thrown if a user not logged in attempts to perform an action that requires a
+ * login.
+ * 
+ * @author Denton Liu
+ * @version 2015-06-01
+ */
 public class NotLoggedInException extends Exception {
-
+	
 	/** Auto-generated. */
 	private static final long serialVersionUID = -8796182387058914431L;
 	
