37e7d13c6186b605efa380c4c2af63d771d7185c
Denton Liu <liu.denton+github@gmail.com>
2015-06-01 01:34:15 -0400

    More javadoc updates.

--- Webchat/src/webchat/users/networking/client/OnlineUserRetriever.java
+++ Webchat/src/webchat/users/networking/client/OnlineUserRetriever.java
@@ -6,10 +6,27 @@ import webchat.networking.GenericRetriever;
 import webchat.ui.ClientUI;
 import webchat.users.networking.UserInterface;
 
+/**
+ * An implemention of {@code Runnable} which fetches online users from the
+ * server.
+ * 
+ * @author Denton Liu
+ * @version 2015-06-01
+ */
 public class OnlineUserRetriever extends GenericRetriever {
 	
+	/** The {@code UserInterface} which will be called. */
 	private final UserInterface userInterface;
 	
+	/**
+	 * 
+	 * @param userInterface
+	 *            The {@code UserInterface} which will be called.
+	 * @param period
+	 *            The time in milliseconds between successive message fetches.
+	 * @param ui
+	 *            The UI which will be updated.
+	 */
 	public OnlineUserRetriever(UserInterface userInterface, long period,
 			ClientUI ui) {
 		super(period, ui);
