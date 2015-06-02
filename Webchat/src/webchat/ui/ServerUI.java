f7b4cc4ada6e5d25dc3346940071c64204499af0
Filip-F <f.francet@hotmail.com>
2015-06-02 00:19:45 -0400

    Fixed a few bugs with stopping the server.

--- Webchat/src/webchat/ui/ServerUI.java
+++ Webchat/src/webchat/ui/ServerUI.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.rmi.AlreadyBoundException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
@@ -36,35 +38,40 @@ import webchat.networking.StartServer;
  * @version 2015-05-25
  */
 public class ServerUI extends Application {
-	/** Image to place in the background.
+	/**
+	 * Image to place in the background.
 	 */
 	private final Image image1 = new Image("/background.jpg", true);
-	/**Imageview node to hold image.
+	/**
+	 * Imageview node to hold image.
 	 */
 	private final ImageView imview = new ImageView();
-	/**File chooser for users to get files from system.*/
+	/** File chooser for users to get files from system. */
 	private final FileChooser fileChooser = new FileChooser();
-	/**Button to open database*/
+	/** Button to open database */
 	private final Button openDatabase = new Button("Open database file");
-	/**Button to create database*/
+	/** Button to create database */
 	private final Button createDatabase = new Button("Create database");
-	/**Button to start server*/
+	/** Button to start server */
 	private final Button startServer = new Button("Start the server");
 
-	/**Text to display file*/
+	/** Text to display file */
 	private Text fileText;
-	/**Text to display error*/
+	/** Text to display error */
 	private Text errorText;
-	/**Text to display*/
+	/** Text to display */
 	private Text selectText;
-	/**Text to display*/
+	/** Text to display */
 	private Text createText;
-	/**The database file*/
+	/** The database file */
 	private File database;
-	/**The variable that works with dragging the interface.*/
+	/** The variable that works with dragging the interface. */
 	private double xvar;
-	/**The variable that works with dragging the interface.*/
+	/** The variable that works with dragging the interface. */
 	private double yvar;
+	/** The boolean to store if the server has been started yet. */
+	private boolean hasStarted = false;
+
 	/**
 	 * Start method for the scene.
 	 */
@@ -76,22 +83,22 @@ public class ServerUI extends Application {
 		this.imview.setImage(this.image1);
 		this.imview.setFitHeight(600);
 		this.imview.setFitWidth(800);
-		
+
 		final VBox vbox = new VBox();
 		vbox.setSpacing(15);
-		
+
 		final HBox hbox1 = new HBox();
 		hbox1.setSpacing(15);
-		
+
 		final HBox hbox2 = new HBox();
 		hbox2.setSpacing(15);
-		
+
 		final HBox hbox3 = new HBox();
 		hbox3.setSpacing(15);
-		
+
 		final GridPane pane = new GridPane();
-		//pane.setAlignment(Pos.CENTER);
-		pane.setPadding(new Insets(0,0,15,15));
+		// pane.setAlignment(Pos.CENTER);
+		pane.setPadding(new Insets(0, 0, 15, 15));
 		pane.add(vbox, 1, 1);
 
 		group.getChildren().add(this.imview);
@@ -104,16 +111,25 @@ public class ServerUI extends Application {
 		close.setStyle("-fx-font-size: 30");
 		min.setStyle("-fx-font-size: 30");
 		HBox titlebar = new HBox();
-		HBox.setMargin(close, new Insets(0,10,0,15));
-		HBox.setMargin(min, new Insets(0,0,0,15));
+		HBox.setMargin(close, new Insets(0, 10, 0, 15));
+		HBox.setMargin(min, new Insets(0, 0, 0, 15));
 		titlebar.setAlignment(Pos.CENTER_RIGHT);
 		titlebar.setPrefWidth(250);
-		titlebar.getChildren().addAll(min,close);
+		titlebar.getChildren().addAll(min, close);
 
 		close.setOnMousePressed(new EventHandler<MouseEvent>() {
 			@Override
 			public void handle(MouseEvent mouseEvent) {
+				if (hasStarted) {
+					try {
+						StartServer.stop();
+					} catch (RemoteException | NotBoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
 				primaryStage.close();
+				System.exit(0);
 			}
 
 		});
@@ -125,12 +141,12 @@ public class ServerUI extends Application {
 			}
 
 		});
-		
+
 		group.setOnMousePressed(new EventHandler<MouseEvent>() {
 			@Override
 			public void handle(MouseEvent event) {
-				xvar= event.getSceneX();
-				yvar= event.getSceneY();
+				xvar = event.getSceneX();
+				yvar = event.getSceneY();
 			}
 		});
 
@@ -142,8 +158,8 @@ public class ServerUI extends Application {
 			}
 		});
 
-		pane.add(titlebar,1,0);
-		
+		pane.add(titlebar, 1, 0);
+
 		Text ipaddress;
 		try {
 			ipaddress = new Text("The server IP address is: "
@@ -154,19 +170,19 @@ public class ServerUI extends Application {
 			e.printStackTrace();
 		}
 		addFileChooser(primaryStage, hbox1, hbox2);
-		
+
 		this.fileText = new Text("No file selected");
 		this.fileText.setId("textstyle2");
 		hbox1.getChildren().add(this.fileText);
-		
+
 		this.createText = new Text("");
 		this.createText.setVisible(false);
 		this.createText.setId("textstyle2");
 		hbox2.getChildren().add(this.createText);
-		
+
 		this.selectText = new Text("Select a file before starting the server");
 		this.selectText.setId("textstyle2");
-		
+
 		vbox.getChildren().addAll(hbox1, hbox2, hbox3);
 		this.startServer.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
@@ -175,21 +191,22 @@ public class ServerUI extends Application {
 					try {
 						StartServer.start(ServerUI.this.database);
 						ServerUI.this.selectText.setText("Server started");
+						hasStarted = true;
 					} catch (final FileNotFoundException e1) {
 						ServerUI.this.errorText
-						.setText("File could not be found");
+								.setText("File could not be found");
 						ServerUI.this.errorText.setVisible(true);
 						e1.printStackTrace();
 					} catch (final ClassNotFoundException e1) {
 						e1.printStackTrace();
 					} catch (final IOException e1) {
 						ServerUI.this.errorText
-						.setText("Database could not be loaded");
+								.setText("Database could not be loaded");
 						ServerUI.this.errorText.setVisible(true);
 						e1.printStackTrace();
 					} catch (final AlreadyBoundException e1) {
 						ServerUI.this.errorText
-						.setText("Server could not be started");
+								.setText("Server could not be started");
 						ServerUI.this.errorText.setVisible(true);
 						e1.printStackTrace();
 					}
@@ -199,37 +216,34 @@ public class ServerUI extends Application {
 				}
 			}
 		});
-		
+
 		hbox3.getChildren().add(this.startServer);
-		
+
 		this.errorText = new Text("");
 		this.errorText.setVisible(false);
 		this.errorText.setId("textstyle2");
 		hbox3.getChildren().add(this.errorText);
-		
+
 		vbox.getChildren().add(this.selectText);
-		
+
 		final Scene scene = new Scene(group, 405, 270);
-		
+
 		scene.getStylesheets().add("/custom.css");
-		
+
 		primaryStage.setScene(scene);
-		primaryStage.getIcons().add(new Image("/ServerIcon.png" ));
+		primaryStage.getIcons().add(new Image("/ServerIcon.png"));
 		primaryStage.show();
 
-		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-			@Override
-			public void handle(WindowEvent we) {
-				System.exit(0);
-			}
-		});
 	}
-	
+
 	/**
 	 * 
-	 * @param stage The stage where the scenes are placed.
-	 * @param hbox1 The horizontal box for layout purposes.
-	 * @param hbox2 The second horizontal box for layout purposes.
+	 * @param stage
+	 *            The stage where the scenes are placed.
+	 * @param hbox1
+	 *            The horizontal box for layout purposes.
+	 * @param hbox2
+	 *            The second horizontal box for layout purposes.
 	 */
 	private void addFileChooser(final Stage stage, HBox hbox1, HBox hbox2) {
 		// TODO check if this works for runnable jars
@@ -251,7 +265,7 @@ public class ServerUI extends Application {
 				}
 			}
 		});
-		
+
 		this.createDatabase.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(final ActionEvent e) {
@@ -272,14 +286,14 @@ public class ServerUI extends Application {
 							.toString()
 							.substring(
 									ServerUI.this.database.toString()
-									.lastIndexOf('.') + 1)
-									.equals("ser")) {
-						
+											.lastIndexOf('.') + 1)
+							.equals("ser")) {
+
 						try {
 							new UserDatabase()
-							.saveDatabase(ServerUI.this.database);
+									.saveDatabase(ServerUI.this.database);
 							ServerUI.this.createText
-							.setText("Database file created");
+									.setText("Database file created");
 							ServerUI.this.createText.setVisible(true);
 							ServerUI.this.fileText.setText("File chosen: "
 									+ ServerUI.this.database.getName());
@@ -291,19 +305,19 @@ public class ServerUI extends Application {
 						}
 					} else {
 						ServerUI.this.createText
-						.setText("Incorrect file format");
+								.setText("Incorrect file format");
 						ServerUI.this.createText.setVisible(true);
 					}
-					
+
 				}
-				
+
 			}
 		});
-		
+
 		hbox1.getChildren().add(this.openDatabase);
 		hbox2.getChildren().add(this.createDatabase);
 	}
-	
+
 	/**
 	 * 
 	 * @param args
