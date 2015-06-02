09e37a4532892602d4ee01cab5ab3f8cc5ed04a1
Filip-F <f.francet@hotmail.com>
2015-06-02 04:11:51 -0400

    Ok one more.

--- Webchat/tests/webchat/test/users/networking/client/UserClientTest.java
+++ Webchat/tests/webchat/test/users/networking/client/UserClientTest.java
@@ -3,38 +3,33 @@ package webchat.test.users.networking.client;
 import java.io.File;
 import java.rmi.RemoteException;
 
-import org.jmock.Expectations;
-import org.jmock.Mockery;
-import org.jmock.lib.concurrent.Synchroniser;
-import org.jmock.lib.legacy.ClassImposteriser;
+
 import org.jmock.integration.junit3.MockObjectTestCase;
+import org.jmock.lib.legacy.ClassImposteriser;
+import org.jmock.Mockery;
 
 import webchat.database.UserDatabase;
 import webchat.ui.ClientUI;
 import webchat.users.networking.client.UserClient;
 import webchat.users.networking.server.UserServer;
-import junit.framework.TestCase;
 
-public class OnlineUserRetrieverTest extends MockObjectTestCase {
-
+public class UserClientTest extends MockObjectTestCase {
 	Mockery context = new Mockery();
-
+	
 	ClientUI ui;
 	UserClient client;
 	UserServer server;
 	UserDatabase database;
 	private File file;
 	private boolean notSetUp = true;
-
+	
 	private String username = "test";
-	private String[] expected = {"test"};
-	private byte[] passwordHash = { 0, 0, 0 };
+	private byte[] passwordHash = {0,0,0};
 	private byte[] userInstance;
-
+	
 	protected void setUp() throws RemoteException, Exception {
 		if (notSetUp) {
 			context.setImposteriser(ClassImposteriser.INSTANCE);
-			context.setThreadingPolicy(new Synchroniser());
 			ui = context.mock(ClientUI.class);
 			file = new File("testFile.ser");
 			file.deleteOnExit();
@@ -44,18 +39,17 @@ public class OnlineUserRetrieverTest extends MockObjectTestCase {
 		this.server = new UserServer(database);
 		server.startServer();
 		client = new UserClient("", ui);
-
 		notSetUp = true;
 	}
-
+	
 	protected void tearDown() {
-		client.stopClient();
 		this.server = null;
 		this.database = null;
 		this.userInstance = null;
 	}
-
-	public void testRecieveMessages() {
+	
+	
+	public void testLogOut(){
 		try {
 			assertTrue(client.register(username, passwordHash));
 		} catch (RemoteException e) {
@@ -68,16 +62,12 @@ public class OnlineUserRetrieverTest extends MockObjectTestCase {
 			e.printStackTrace();
 			fail();
 		}
-
-		checking(new Expectations() {
-			{
-				oneOf(ui).writeUsers(expected);
-			}
-		});
-
-		client.startClient();
-		
-		client.refreshUsers();
+		try {
+			client.logout(userInstance);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+			fail();
+		}
 	}
 
 }
