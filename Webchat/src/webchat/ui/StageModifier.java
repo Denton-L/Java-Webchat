5d31e4a57fc2f115fbf91f45118de8ffeb73826d
Filip-F <f.francet@hotmail.com>
2015-06-01 23:40:33 -0400

    Fixed bug.

--- Webchat/src/webchat/ui/StageModifier.java
+++ Webchat/src/webchat/ui/StageModifier.java
@@ -116,8 +116,8 @@ public class StageModifier {
        close.setOnMousePressed(new EventHandler<MouseEvent>() {
 			@Override
 			public void handle(MouseEvent mouseEvent) {
-				stage.close();
 				System.exit(0);
+				stage.close();
 			}
 
 		});
